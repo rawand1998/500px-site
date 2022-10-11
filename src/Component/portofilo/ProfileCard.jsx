aef9da84c3a90eb9db255d7e1eecbaf4601c25cb
rawand1998 <rawandgaradh@gmail.com>
2022-10-11 09:07:02 +0300

    edit profile

--- src/Component/portofilo/ProfileCard.jsx
+++ src/Component/portofilo/ProfileCard.jsx
@@ -1,6 +1,7 @@
 import "./cardstyle.css";
-const CardPhoto = ({ d, title, para, btn1, btn2 }) => (
-  <div className="cardPhoto">
+const CardPhoto = ({ d, title, para, btn1, btn2 ,className}) => (
+  <div className={className}>
+   
     <svg height="40" width="40" fill="#fff" viewBox="0 0 40 40">
       <path d={d}></path>
     </svg>
@@ -10,16 +11,18 @@ const CardPhoto = ({ d, title, para, btn1, btn2 }) => (
     {btn2}
   </div>
 );
-const CardGalleries = ({ d, title, para, btn }) => {
+
+
+const CardGalleries = ({ d, title, para, btn,classBtn }) => {
   return (
     <div className="card_catergory">
-      <svg height="64" width="64" fill="#000" viewBox="0 0 24 24">
+      <svg height="48" width="48" fill="#000" viewBox="0 0 24 24">
         <path d={d}></path>
       </svg>
       <span className="para_cat">{title}</span>
       <span className="para_cats">{para}</span>
 
-      <button className="cat_btn button_profile">{btn}</button>
+      <button className={classBtn}>{btn}</button>
     </div>
   );
 };
